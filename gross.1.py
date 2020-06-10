1583a80f5fe67bffc87d262b73f02f6aa01da214
Princekumar1999 <61105671+Princekumar1999@users.noreply.github.com>
2020-06-10 15:41:44 +0530

    Update gross.1.py

--- gross.1.py
+++ gross.1.py
@@ -3,12 +3,16 @@
  Pay the hourly rate for the hours up to 40 and 1.5 times
  the hourly rate for all hours worked above 40 hours.
  Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75)'''
-hrs = float(input('Enter Hours:'))
-rat = float(input('Enter Rate:'))
-if hrs>40:
-    sdk = hrs * rat
-    hgg = (hrs - 40.0) * (rat * 0.5)
-    hgs = sdk + hgg
-else:
-    hgs = hrs * rat
-print(hgs)
+try:
+    hrs = float(input('Enter Hours:'))
+    rat = float(input('Enter Rate:'))
+    if hrs>40:
+        sdk = hrs * rat
+        hgg = (hrs - 40.0) * (rat * 0.5)
+        hgs = sdk + hgg
+    else:
+        hgs = hrs * rat
+    print(hgs)
+except:
+    print('Error, Enter numeric value:')
+ 
